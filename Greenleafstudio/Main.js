207a661dee2cdf0ff68a2c2f67d7afda936f068a
Prajwal Shrestha <prajwalstha9841@gmail.com>
2025-01-21 20:33:25 +0545

    Add files via upload

---
+++ Greenleafstudio/Main.js
@@ -0,0 +1,16 @@
+function change()
+{
+  let changetext = document.getElementById('textbox').innerHTML='Front-End Developmen We specialize in creating user-friendly and visually appealing interfaces that bring your ideas to life. Our front-end development team ensures your website or application looks great and works seamlessly across all devices. With a focus on responsiveness and usability, we deliver digital experiences that leave a lasting impression.';
+}
+function change1()
+{
+  let changetext1 = document.getElementById('textbox').innerHTML="How are you";
+}
+function change2()
+{
+  let changetext2 = document.getElementById('textbox').innerHTML='Hello world!';
+}
+function change3()
+{
+  let changetext3 = document.getElementById('textbox').innerHTML='Hello world!';
+}
